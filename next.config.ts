42da3814971c6c8cc9b8a5b38cded02cae7962fa
Shazam <shazajmal37@gmail.com>
2025-10-22 13:15:12 +0530

    another push

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone', // Optimized for deployment
+  reactStrictMode: true,
 };
 
 export default nextConfig;
